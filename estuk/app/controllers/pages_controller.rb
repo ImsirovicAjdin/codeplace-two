7f1aa9c108fe79c2a755d2efc7427c6e5d63bf49
ajdin <ajdika@live.com>
2017-03-26 08:56:34 +0000

    Create the dashboard

--- estuk/app/controllers/pages_controller.rb
+++ estuk/app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
+  # make sure only logged in users can access the dashboard.
+  before_action :authenticate_user!, only: [:dashboard]
+  
   def home
     if current_user
       redirect_to books_path
     end
+  end
+  
+  def dashboard
+    @books = current_user.books
   end
 end
